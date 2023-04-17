1350938374c505f91b3a70adf3305e0bdf16669c
Nandhini Kirubakaran <knaphasri@gmail.com>
2023-04-17 14:08:34 +0530

    add

--- src/Home.js
+++ src/Home.js
@@ -40,7 +40,7 @@ function LoginForm() {
           console.log("❌ Error");
           setFormState("error");
         } else {
-          const result = await data.json();
+          const result = data.json();
           console.log("✅ Success", result);
           navigate("/class-details");
         }
